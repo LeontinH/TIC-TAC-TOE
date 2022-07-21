a40b7a1ee55fbd5b8c0a08d8b67f053425e60c7a
LeontinH <107749298+LeontinH@users.noreply.github.com>
2022-07-21 18:34:35 +0300

    Update ticTacToe.js

--- ticTacToe.js
+++ ticTacToe.js
@@ -17,7 +17,7 @@ let freeCells = 9;
 
 function fillInCells(cell) {
   cell.addEventListener("click", function() {
-    if(cell.innerHTML == ""){
+    if(cell.innerHTML == "" && freeCells != -1){
       cell.innerHTML = nextPlayer;
       --freeCells;
     }     
@@ -26,17 +26,16 @@ function fillInCells(cell) {
 
 function changePlayer(cell) {
   cell.addEventListener("click", function() {
-      if(nextPlayer == "X"){
-        nextPlayer = "O";
-      } else {
-        nextPlayer = "X";
-      }
+    if(nextPlayer == "X"){
+      nextPlayer = "O";
+    } else {
+      nextPlayer = "X";
+    }
   });
 }
  
 function checkTheWinner(cell) {
   cell.addEventListener("click", function() {
-    let theWinner = "";
     let cell1 = document.getElementById("cellNo0").innerHTML;
     let cell2 = document.getElementById("cellNo1").innerHTML;
     let cell3 = document.getElementById("cellNo2").innerHTML;
@@ -48,31 +47,21 @@ function checkTheWinner(cell) {
     let cell9 = document.getElementById("cellNo8").innerHTML;
    
     if ((((cell1 == cell2) && (cell2 == cell3)) || ((cell1 == cell4) && (cell4 == cell7))) && (cell1 != "")) {
-      theWinner = cell1;
-    }
-
-    if ((((cell3 == cell6) && (cell6 == cell9)) || ((cell7 == cell8) && (cell8 == cell9))) && (cell9 != "")) {
-      theWinner = cell9;
-    }
-   
-    if ((((cell1 == cell5) && (cell5 == cell9)) || ((cell3 == cell5) && (cell5 == cell7)) || ((cell4 == cell5) && 
+      print("The winner is the player played with: " + cell1);
+    } else if ((((cell3 == cell6) && (cell6 == cell9)) || ((cell7 == cell8) && (cell8 == cell9))) && (cell9 != "")) {
+      print("The winner is the player played with: " + cell9);
+    } else if ((((cell1 == cell5) && (cell5 == cell9)) || ((cell3 == cell5) && (cell5 == cell7)) || ((cell4 == cell5) && 
       (cell5 == cell6)) || ((cell2 == cell5) && (cell5 == cell8))) && (cell5 != "")) {
-      theWinner = cell5;
-    }
-
-    if (theWinner != "") {
-      print("The winner is the player played with: " + theWinner);
-      document.getElementById("startAgain").style.visibility = "visible";
-    }
-
-    if (theWinner == "" && freeCells == 0) {
+      print("The winner is the player played with: " + cell5);
+    } else if (freeCells == 0) {
       print("It is a draw!");
-      document.getElementById("startAgain").style.visibility = "visible";
     }
   });
 }
 
 function print(str) {
+  freeCells = -1;
+  document.getElementById("startAgain").style.visibility = "visible";
   return document.getElementById("mesageElement1").innerHTML = str;
 }
 
